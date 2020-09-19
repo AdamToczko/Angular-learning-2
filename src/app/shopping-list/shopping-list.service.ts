8e5571ac3ad7fed9caf37a71b7ca0089b9533b40
AdamToczko <adamtoczko80@gmail.com>
2020-09-19 08:10:26 +0200

    adding ingredients to recipes and passing to shopping list via service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,28 +1,32 @@
 import { EventEmitter, Injectable } from '@angular/core';
 
-import { Ingredient } from "../shared/ingredient.model";
+import { Ingredient } from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShoppingListService {
   //as in getIngredients there is only copy we need to update with new available data
-  ingredientsChanged = new EventEmitter<Ingredient[]>()
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
 
-  private ingredients: Ingredient[] = [
-    new Ingredient("Apples", 5),
-    new Ingredient("Tomatoes", 10),
-];
+  private ingredients: Ingredient[] = [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)];
 
-  constructor() { }
+  constructor() {}
 
   getIngredients() {
     return this.ingredients.slice();
-}
+  }
 
-addIngredient(ingredient: Ingredient){
-  this.ingredients.push(ingredient)
-  this.ingredientsChanged.emit(this.ingredients.slice())
-}
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 
+  addIngredients(ingredients: Ingredient[]) {
+    // for (let ingredient of ingredients) {
+    //   this.addIngredient(ingredient);
+    // } this option not optimal
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
