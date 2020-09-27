27e81878ce8ebc9a3d178e4e8b797cdb861759bf
AdamToczko <adamtoczko80@gmail.com>
2020-09-27 11:40:45 +0200

    shopping list form

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -11,6 +11,7 @@ export class ShoppingListService {
   // ingredientsChanged = new EventEmitter<Ingredient[]>();
   // better solution with Subject instead of EventEmitter:
   ingredientsChanged = new Subject<Ingredient[]>();
+  startedEditing = new Subject<number>();
 
   private ingredients: Ingredient[] = [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)];
 
@@ -18,6 +19,10 @@ export class ShoppingListService {
 
   getIngredients() {
     return this.ingredients.slice();
+  }
+
+  getIngredient(index: number) {
+    return this.ingredients[index];
   }
 
   addIngredient(ingredient: Ingredient) {
@@ -34,6 +39,16 @@ export class ShoppingListService {
     this.ingredients.push(...ingredients);
     // this.ingredientsChanged.emit(this.ingredients.slice());
     // better solution with Subject
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  updateIngredient(index: number, newIngredient: Ingredient) {
+    this.ingredients[index] = newIngredient;
+    this.ingredientsChanged.next(this.ingredients.slice());
+  }
+
+  deleteIngredient(index: number) {
+    this.ingredients.splice(index, 1);
     this.ingredientsChanged.next(this.ingredients.slice());
   }
 }
